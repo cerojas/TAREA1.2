6c33f7215131e4452905c5bedd76796bc6991c4b
cerojas <cerojas09@gmail.com>
2012-09-10 10:59:46 -0400

    primera clase perro

---
+++ Granja.java
@@ -0,0 +1,11 @@
+// esta es la clase principal
+
+public class Granja
+{
+	public static void main(String[] args)
+	{
+		Perro perro =  new Perro();
+		
+		perro.decirQuienSoy();
+	}
+}
---
+++ Perro.java
@@ -0,0 +1,9 @@
+// esta es la clase perro
+
+public class Perro
+{
+	public void decirQuienSoy()
+	{
+		System.out.println("Perro");
+	}
+}
